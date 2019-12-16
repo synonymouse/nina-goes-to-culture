468c0c771f903181a74ff3a8dcb608fda5603574
Elisey Shchukin <e.shchukin@ailove.ru>
2019-12-16 20:43:40 +0300

    Updates

---
+++ precache-manifest.f3042c5d1512117d1ccae7bd2676bedc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a0e223229d36f82a19082ee46c6a9ab",
+    "url": "/nina-goes-to-web/index.html"
+  },
+  {
+    "revision": "8fd45681255917b66e18",
+    "url": "/nina-goes-to-web/static/css/main.d9d64b85.chunk.css"
+  },
+  {
+    "revision": "46c59b91f0f6dd0b183e",
+    "url": "/nina-goes-to-web/static/js/2.81616c33.chunk.js"
+  },
+  {
+    "revision": "8fd45681255917b66e18",
+    "url": "/nina-goes-to-web/static/js/main.21b3951a.chunk.js"
+  },
+  {
+    "revision": "b305ac3c8b800b226a7a",
+    "url": "/nina-goes-to-web/static/js/runtime-main.06b4cdd5.js"
+  }
+]);
